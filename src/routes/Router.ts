9397a62426b07fdeed7d7cca08d9a75e4566120c
AnteVol <ante.volarevic@fer.hr>
2024-10-28 02:53:54 +0100

    added auth

--- src/routes/Router.ts
+++ src/routes/Router.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { createTicket, getTicketById, totalTicketCount } from '../controllers/tikcetController';
 import { TokenValidation } from '../middlewares/Auth';
+const { requiresAuth } = require('express-openid-connect');
 
 const router = Router();
 
 router.get('/', totalTicketCount);
 router.post('/ticket', TokenValidation, createTicket);
-router.get('/ticket/:id', TokenValidation, getTicketById);
+router.get('/ticket/:id', requiresAuth(), getTicketById);
 
 export default router;
