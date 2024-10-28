f3b4f7de5bf0b500f4c5d375c960b66605b9b393
AnteVol <ante.volarevic@fer.hr>
2024-10-28 04:39:42 +0100

    new3

--- src/config/Auth0.ts
+++ src/config/Auth0.ts
@@ -8,5 +8,5 @@ export const authConfig = {
   secret: process.env.SECRET,
   baseURL: process.env.BASE_URL,
   clientID: process.env.CLIENT_ID,
-  issuerBaseURL: process.env.ISSUERBASEURL
+  issuerBaseURL: process.env.IS_SUERBASE_URL
 };
